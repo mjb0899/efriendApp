9f5c074916a6e730b7cdc982d1a93bc723f46f2f
ADMIN <morrison0899@gmail.com>
2017-07-21 13:29:56 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ADMIN
+ * Date: 06/07/2017
+ * Time: 20:58
+ */
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+Hi Morrison. You are about to create something amazing!! This is the new project
+<?php echo 'hey'?>
+</body>
+</html>
