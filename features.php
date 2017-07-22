ef6cd93fe253217509c441782f64ab82718482bc
ADMIN <morrison0899@gmail.com>
2017-07-22 22:29:18 +0100

    Inital Commits

---
+++ features.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="main.css">
+
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-inverse navbar-fixed-top menu_logo">
+        <div class="container-fluid">
+            <div class="navbar-header menu_logo">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand menu_logo" href="#" >eFriend</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Features <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">eFriend Finder</a></li>
+                            <li><a href="#">eConnect</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Happy Friends</a></li>
+                    <li><a href="#">About Us</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+<main>
+    <div class="container">
+        <div class="wrapper_left">
+            <div class="eFriendLogo"></div>
+            efriend
+        </div>
+
+        <div class="wrapper_right">
+
+            <div class="eConnectLogo"></div>
+            eConnect
+
+
+        </div>
+    </div>
+</main>
+<footer>
+    <footer>
+        <div class="footer_left" >
+            <h1>eFriend</h1>
+        </div>
+        <div class="footer_right" >
+            <a target="_blank" title="follow me on facebook" href="http://www.facebook.com/PLACEHOLDER"><img alt="follow me on facebook" src="https://c866088.ssl.cf3.rackcdn.com/assets/facebook30x30.png" border=0></a>
+            <a target="_blank" title="follow me on Twitter" href="http://www.twitter.com/PLACEHOLDER"><img alt="follow me on Twitter" src="https://c866088.ssl.cf3.rackcdn.com/assets/twitter30x30.png" border=0></a>
+            <a target="_blank" title="follow me on instagram" href="http://www.instagram.com/PLACEHOLDER"><img alt="follow me on instagram" src="https://c866088.ssl.cf3.rackcdn.com/assets/instagram30x30.png" border=0></a>
+        </div>
+        <div class="footer_below" >
+            <h5>Copyright &copy; 2017 &bull; All rights reserved &bull; Music&me.com</h5>
+        </div>
+    </footer>
+</footer>
+</body>
+</html>
