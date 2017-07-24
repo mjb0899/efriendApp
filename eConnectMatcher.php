8469de1cc044c3dad8c80f6470993939b5019e9c
ADMIN <morrison0899@gmail.com>
2017-07-24 23:03:40 +0100

    Inital Commits

---
+++ eConnectMatcher.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ADMIN
+ * Date: 24/07/2017
+ * Time: 22:53
+ */
+
+session_start();
+if(isset($_SESSION['name'])){
+    $username=$_SESSION['name'];
+}else{
+    header("url=pageNotFound.php");
+
+}
+?>
