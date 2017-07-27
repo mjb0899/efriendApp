dbcd397f546886d107fe9a7fa5d257ee9d7b752a
ADMIN <morrison0899@gmail.com>
2017-07-27 15:30:42 +0100

    Inital Commits

--- eFriendSurvey.php
+++ eFriendSurvey.php
@@ -76,28 +76,31 @@
 
     </style>
     <script>
-       $(document).ready(function () {
-            $("submit").click(function () {
-                var like1 =document.getElementById('like1').value;
-                var like2 =document.getElementById('like2').value;
-               var like3 =document.getElementById('like3').value;
-                var like4 =document.getElementById('like4').value;
-                var like5 =document.getElementById('like5').value;
-                document.getElementById('likes').value=like1+'#'+like2+'#'+like3+'#'+like4+'#'+like5;
-           })
-        })
+    //   $(document).ready(function () {
+    //        $("submit").click(function () {
+    //            var like1 =document.getElementById('like1').value;
+    //            var like2 =document.getElementById('like2').value;
+    //           var like3 =document.getElementById('like3').value;
+    //            var like4 =document.getElementById('like4').value;
+    //            var like5 =document.getElementById('like5').value;
+    //            document.getElementById('likes').value=like1+'#'+like2+'#'+like3+'#'+like4+'#'+like5;
+   //        })
+    //    })
 
     </script>
 
- <!--   <script type="text/javascript">
+    <script type="text/javascript">
         function join_ym()
         {
             var like1 =document.getElementById('like1').value;
             var like2 =document.getElementById('like2').value;
+            var like3 =document.getElementById('like3').value;
+            var like4 =document.getElementById('like4').value;
+            var like5 =document.getElementById('like5').value;
             document.getElementById('likes').value=like1+'#'+like2+'#'+like3+'#'+like4+'#'+like5;
-            alert(document.getElementById('likes').value);
+        //    alert(document.getElementById('likes').value);
         }
-    </script>-->
+    </script>
 
 
 </head>
