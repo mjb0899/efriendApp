b76879a636b3d0c5a2db9d22fdff425956ef3d55
ADMIN <morrison0899@gmail.com>
2017-08-03 14:45:14 +0100

    Inital Commits

--- userProfile.php
+++ userProfile.php
@@ -208,10 +208,10 @@ if(isset($_SESSION['name'])){
                 </div>
                 <div id="menu1" class="tab-pane fade">
                     <h3>Search Info</h3>
-                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>Show and update search Information like sex,place</p>
                 </div>
                 <div id="menu2" class="tab-pane fade">
-                    <h3>Menu 2</h3>
+                    <h3>Images</h3>
                     <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
                 </div>
                 <div id="menu3" class="tab-pane fade">
