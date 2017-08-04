4b0becb02dd1c88d1799b4805181e5f57a71a276
ADMIN <morrison0899@gmail.com>
2017-08-04 14:00:54 +0100

    Inital Commits

--- eFriendMatcher.php
+++ eFriendMatcher.php
@@ -87,15 +87,15 @@ if(isset($_SESSION['name'])){
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
                 <div class="item active">
-                    <img src="images/rr2.png" id="#pic" alt="Los Angeles" style="width:100%;">
+                    <img src="images/rr2.png" id="#pic" alt="Los Angeles" >
                 </div>
 
                 <div class="item">
-                    <img src="images/rr2.png" id="#pic" alt="Chicago" style="width:100%;">
+                    <img src="images/rr2.png" id="#pic" alt="Chicago" >
                 </div>
 
                 <div class="item">
-                    <img src="images/rr2.png" id="#pic" alt="New york" style="width:100%;">
+                    <img src="images/rr2.png" id="#pic" alt="New york" >
                 </div>
             </div>
 
