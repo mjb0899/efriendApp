fb0d1ca0f50b9edd2be65b20805465284a01666a
ADMIN <morrison0899@gmail.com>
2017-08-07 15:38:39 +0100

    Inital Commits

--- userProfile.php
+++ userProfile.php
@@ -110,6 +110,35 @@ if(isset($_SESSION['name'])){
             <div class="profile_image">
                 <img id="pic" src="images/rr2.png">
             </div>
+
+            <?php
+            if(isset($_SESSION['match'])){
+                echo '<div class="container">
+                                <!-- Trigger the modal with a button -->
+                                <button type="button" class="btn-place" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-pencil"></button>
+                                <!-- Modal -->
+                                <div class="modal fade" id="myModal" role="dialog">
+                                    <div class="modal-dialog modal-sm">
+                                         <div class="modal-content">
+                                            <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                <h4 class="modal-title">Upload Profile Image</h4>
+                                            </div>
+                                             <div class="modal-body">
+                                                <form action="uploadImage.php" method="post" enctype="multipart/form-data">
+                                                <input type="file" name="file" id="exampleInputFile" required>
+                                                <button type="submit" class="btn btn-default btn-sm" name="submit"> <span class="glyphicon glyphicon-pencil">Upload</span></button>
+                                                </form>
+                                             </div>
+                                            <div class="modal-footer">
+                                                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                            </div>
+                                         </div>
+                                 </div>
+                                </div>
+                            </div>';
+            }
+            ?>
             <h4>Morrison Barreto</h4>
         </div>
         <div class="div_middle">
