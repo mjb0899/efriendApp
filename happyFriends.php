72536866ab8ec65d4c8691d264e9777a18515ca7
ADMIN <morrison0899@gmail.com>
2017-08-07 15:17:57 +0100

    Inital Commits

--- happyFriends.php
+++ happyFriends.php
@@ -5,3 +5,113 @@
  * Date: 07/08/2017
  * Time: 14:53
  */
+session_start();
+?>
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <title>MusicAndMe</title>
+    <!--MENUBAR CSS-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+<!--HEADER CODE-->
+<header>
+    <nav class="navbar navbar-inverse navbar-fixed-top menu_logo">
+        <div class="container-fluid">
+            <div class="navbar-header menu_logo">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand menu_logo" href="index.php" >eFriend</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+
+
+
+                    <?php if(isset($_SESSION['name'])) {   echo"    <li><a href='features.php'>Home</a></li>"; }  ?>
+
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Features <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+
+                            <?php if(isset($_SESSION['name'])) {   echo"       <li><a href='features.php'>eFriend Finder</a></li>"; }  ?>
+                            <?php if(!isset($_SESSION['name'])) {   echo"       <li><a href='efriendAd.php'>eFriend Finder</a></li>"; }  ?>
+
+
+                            <?php if(isset($_SESSION['name'])) {   echo"           <li><a href='eFriendSurvey.php'>eConnect</a></li>"; }  ?>
+                            <?php if(!isset($_SESSION['name'])) {   echo"           <li><a href='econnectAd.php'>eConnect</a></li>"; }  ?>
+
+
+                        </ul>
+                    </li>
+                    <li><a href="happyFriends.php">Happy Friends</a></li>
+                    <li><a href="aboutUs.php">About Us</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <?php if(!isset($_SESSION['name'])) {   echo"    <li><a href='registration.php'><span class='glyphicon glyphicon-user'></span> Sign Up</a></li> "; }  ?>
+                    <?php if(!isset($_SESSION['name'])) {   echo"  <li><a href='index.php'><span class='glyphicon glyphicon-log-in'></span> Login</a></li> "; }  ?>
+                    <?php if(isset($_SESSION['name'])) {   echo'     
+                                                                                                         
+                                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="">'. $_SESSION["name"].' <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+
+                             <li><a href="userProfile.php"><span class="glyphicon glyphicon-th-list"></span>My Profile</a></li>
+                             <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
+
+
+                       </ul>
+                    </li>                                                         
+                                                                                                         
+                                                                                                         '; }  ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+<!--MAIN CODE-->
+<main>
+
+
+
+
+    <div class="container_reg">
+        <div class="wrapper_up">
+
+
+
+
+
+        </div>
+    </div>
+
+
+
+
+</main>
+<!--FOOTER CODE!!-->
+<footer>
+    <div class="footer_left" >
+        <h1>eFriend</h1>
+    </div>
+    <div class="footer_right" >
+        <a target="_blank" title="follow me on facebook" href="http://www.facebook.com/PLACEHOLDER"><img alt="follow me on facebook" src="https://c866088.ssl.cf3.rackcdn.com/assets/facebook30x30.png" border=0></a>
+        <a target="_blank" title="follow me on Twitter" href="http://www.twitter.com/PLACEHOLDER"><img alt="follow me on Twitter" src="https://c866088.ssl.cf3.rackcdn.com/assets/twitter30x30.png" border=0></a>
+        <a target="_blank" title="follow me on instagram" href="http://www.instagram.com/PLACEHOLDER"><img alt="follow me on instagram" src="https://c866088.ssl.cf3.rackcdn.com/assets/instagram30x30.png" border=0></a>
+    </div>
+    <div class="footer_below" >
+        <h5>Copyright &copy; 2017 &bull; All rights reserved &bull; Music&me.com</h5>
+    </div>
+</footer>
+</body>
+</html>
