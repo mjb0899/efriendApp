deb63aea5b131f28e3cfc39b263646c55cbe670d
ADMIN <morrison0899@gmail.com>
2017-08-07 15:59:26 +0100

    Inital Commits

--- userProfile.php
+++ userProfile.php
@@ -111,13 +111,13 @@ if(isset($_SESSION['name'])){
 
                 <?php
                 include("dbConnect.php");
-                $sql_query = "Select profile_image,uusername from users Where username='$owner'";
+                $sql_query = "Select profile_image,uusername from users Where uusername='$username'";
                 $result = $db -> query($sql_query);
                 while($row = $result -> fetch_array()){
                     $profile_path=$row['profile_image'];
                     $user=$row['uusername'];
 //upload profile picture
-                    echo "<img src=\"$profile_path\" id='pic'>";
+                    echo "<img id='pic' src=\"$profile_path\" >";
                 }
                 ?>
 
