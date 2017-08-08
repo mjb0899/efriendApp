eed204e37a7522ce83e03a848d0a6e188c7f9403
ADMIN <morrison0899@gmail.com>
2017-08-08 17:04:33 +0100

    Inital Commits

--- messages.php
+++ messages.php
@@ -87,14 +87,10 @@ session_start();
 
     <div class="container_reg">
         <div class="wrapper_message">
-            <h2>-Our Mission-</h2>
+            <h2>-Your Inbox-</h2>
 
-            <p>
-                Ass the world’s largest music and audio platform, SoundCloud lets people discover and enjoy the greatest selection of music from the most diverse creator community on earth. Since launching in 2008, the platform has become renowned for its unique content and features, including the ability to share music and connect directly with artists, as well as unearth breakthrough tracks, raw demos, podcasts and more. This is made possible by an open platform that directly connects creators and their fans across the globe. Music and audio creators use SoundCloud to both share and monetise their content with a global audience, as well as receive detailed stats and feedback from the Music&Me community.
-            </p>
-            <p>
-                As the world’s largest music and audio platform, SoundCloud lets people discover and enjoy the greatest selection of music from the most diverse creator community on earth. Since launching in 2008, the platform has become renowned for its unique content and features, including the ability to share music and connect directly with artists, as well as unearth breakthrough tracks, raw demos, podcasts and more. This is made possible by an open platform that directly connects creators and their fans across the globe. Music and audio creators use SoundCloud to both share and monetise their content with a global audience, as well as receive detailed stats and feedback from the Music&Me community.
-            </p>
+
+
 
         </div>
     </div>
