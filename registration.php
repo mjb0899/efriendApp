8f88f616216f5521eeb679f7e2288419653616ac
ADMIN <morrison0899@gmail.com>
2017-08-08 14:12:06 +0100

    Inital Commits

--- registration.php
+++ registration.php
@@ -42,8 +42,8 @@
                     <li><a href="#">About Us</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><a href="registration.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="index.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                 </ul>
             </div>
         </div>
