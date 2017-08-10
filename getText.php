81ad6d3dd99a1e17d4d58900025ec1efdefffa45
ADMIN <morrison0899@gmail.com>
2017-08-10 14:18:51 +0100

    reconstruct 1 setting mid=1

--- getText.php
+++ getText.php
@@ -6,22 +6,26 @@
  * Time: 13:53
  */
 
-$message = $_POST['persondata'];
+$message = $_POST['persondata'];//working
 
 session_start();
-$_SESSION['check']=$message;
+$_SESSION['check']=$message; //working
+
 //retrieve mid here and send back to message.j
+
 $uid=$_SESSION['userNum'];
 
-echo 1;
-/*
 
+echo 1;
+
+/*
 $sql_query = "Select mid from message Where uid='$uid' AND match_uname='$message'";
 $result = $db -> query($sql_query);
 while($row = $result -> fetch_array()) {
     $_SESSION['mid'] = $row['mid'];
-}
+}*/
 
+$_SESSION['mid']="1";
 
 
 if($message!=null){
