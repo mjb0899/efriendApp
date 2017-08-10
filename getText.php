a882c1f094aa94a27d1172c5fa6cf1ab4dd30faa
ADMIN <morrison0899@gmail.com>
2017-08-10 22:59:32 +0100

    reconstruct 3 making mid work

--- getText.php
+++ getText.php
@@ -10,7 +10,7 @@ session_start();
 $message = $_POST['persondata'];//working
 
 $_SESSION['check']=$message; //working
-$test=$_SESSION['check'];
+$test =$_POST['persondata'];
 
 //retrieve mid here and send back to mmessage.j
 
