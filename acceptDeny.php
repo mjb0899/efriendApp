17ba3332c238eedb8156d221b353aa29ec73ea02
ADMIN <morrison0899@gmail.com>
2017-08-10 15:23:40 +0100

    reconstruct 2 prepared statement for textGetter

--- acceptDeny.php
+++ acceptDeny.php
@@ -29,12 +29,6 @@ $resp=$_POST['resp'];
 $date = date('Y-m-d H:i:s');
 
 
-//$user=$_SESSION['user'];
-//$artist=$_SESSION['artist'];
-//$cuser=$_POST['cuser'];
-//$muser=$_POST['muser'];
-//$resp=$_POST['resp'];
-
 //connect to db
 include("dbConnect.php");
 
@@ -51,17 +45,43 @@ if(isset($_POST['resp'])){
             echo"Error:".$sql."<br>" . mysqli_error($db);
         }*/
         //prepared
-        $stmt1 = $db->prepare("INSERT into accept(cuser,muser,matchdate) VALUES (?,?,?)");
-        $stmt1->bind_param('sss', $sess, $match,$date);
-        $stmt1->execute();
-        $stmt1->store_result();
-        $stmt1->bind_result($col1);
 
-        $stmt1 = $db->prepare("INSERT into message(uid,match_uname,start_date) VALUES (?,?,?)");
-        $stmt1->bind_param('iss', $uid, $match,$date);
-        $stmt1->execute();
-        $stmt1->store_result();
-        $stmt1->bind_result($col1);
+
+        $stmt = $db->prepare("Select aid from accept Where cuser= ? and muser= ?");
+        $stmt->bind_param('ss', $_SESSION['name'], $match);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($col3);
+
+        while ($stmt->fetch()) {
+
+            if($col3==null){
+                $stmt1 = $db->prepare("INSERT into accept(cuser,muser,matchdate) VALUES (?,?,?)");
+                $stmt1->bind_param('sss', $sess, $match,$date);
+                $stmt1->execute();
+                $stmt1->store_result();
+                $stmt1->bind_result($col1);
+
+                $stmt1 = $db->prepare("INSERT into message(uid,match_uname,start_date) VALUES (?,?,?)");
+                $stmt1->bind_param('iss', $uid, $match,$date);
+                $stmt1->execute();
+                $stmt1->store_result();
+                $stmt1->bind_result($col1);
+
+            }else{
+                echo '<script language="javascript">';
+                echo 'alert("match exists")';
+                echo '</script>';
+            }
+
+
+        }
+
+
+
+
+
+
 
 
 
