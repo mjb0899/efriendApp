e810b3c6218c6ec442d89db54715f3136d4c771a
ADMIN <morrison0899@gmail.com>
2017-08-11 23:10:16 +0100

    making algo work

--- eFriendAlgo.php
+++ eFriendAlgo.php
@@ -15,47 +15,56 @@ $username=$_SESSION['name']; //set current username
 $uid=$_SESSION['userNum'];//get uid
 $_SESSION['match']='sarah10'; //get match
 include("dbConnect.php");
-echo"PART 1 - CREATE BASE ARRAY --> ";
-$base= array();
-$sql = "SELECT ssex,ssmoke,sdrink,sreligion,sage from user_search where uid='$uid'";
-$result=$db->query($sql);
-while($row = $result -> fetch_array()){
-    echo "SEX: ".$row["ssex"]. " /SMOKE ".$row["ssmoke"]. " /DRINK".$row["sdrink"]." /RELIGION".$row["sreligion"]." /AGE".$row["sage"];
-    $ssex=$row["ssex"];
-    $ssmoke=$row["ssmoke"];
-    $sdrink=$row["sdrink"];
-    $sreli=$row["sreligion"];
-    $sage=$row["sage"];
+try{
+
+    echo"PART 1 - CREATE BASE ARRAY --> ";
+    $base= array();
+    $sql = "SELECT ssex,ssmoke,sdrink,sreligion,sage from user_search where uid='$uid'";
+    $result=$db->query($sql);
+    while($row = $result -> fetch_array()) {
+        echo "SEX: " . $row["ssex"] . " /SMOKE " . $row["ssmoke"] . " /DRINK" . $row["sdrink"] . " /RELIGION" . $row["sreligion"] . " /AGE" . $row["sage"];
+        $ssex = $row["ssex"];
+        $ssmoke = $row["ssmoke"];
+        $sdrink = $row["sdrink"];
+        $sreli = $row["sreligion"];
+        $sage = $row["sage"];
 //push sex
-    if($ssex=="z"){
-    }else{
-        $base["sex"]=$ssex;
-    }//end sex
+        if ($ssex == "z") {
+        } else {
+            $base["sex"] = $ssex;
+        }//end sex
 //push smoke
-    if($ssmoke=="_1"){                     //if-2e
-    }else{
-        $base["smoke"]=$ssmoke;
-        //  array_push($base,"s");
-        //   array_push($base,$ssmoke);
-    }
-    //push drink
-    if($sdrink=="_1"){                    //if-3
-    }else{
-        $base["drink"]=$sdrink;
-        //array_push($base,"d");
-        //   array_push($base,$sdrink);
-    }//end drink
-    if($sreli=="_1"){                               //if-4
-    }else{
-        $base["reli"]=$sreli;
-        // array_push($base,"r");
-        //   array_push($base,$sreli);
-    }//end religion..
-    $base["age"]=$sage;
+        if ($ssmoke == "_1") {                     //if-2e
+        } else {
+            $base["smoke"] = $ssmoke;
+            //  array_push($base,"s");
+            //   array_push($base,$ssmoke);
+        }
+        //push drink
+        if ($sdrink == "_1") {                    //if-3
+        } else {
+            $base["drink"] = $sdrink;
+            //array_push($base,"d");
+            //   array_push($base,$sdrink);
+        }//end drink
+        if ($sreli == "_1") {                               //if-4
+        } else {
+            $base["reli"] = $sreli;
+            // array_push($base,"r");
+            //   array_push($base,$sreli);
+        }//end religion..
+        $base["age"] = $sage;
 //array_push($base,$sage);
-    echo '<br>';
-    echo"BASE ARRAY CREATED --> ";print_r($base);
-}
+        echo '<br>';
+        echo "BASE ARRAY CREATED --> ";
+        print_r($base);
+    }
+    }catch(PDOException $exc){
+        echo "error";
+    }
+
+
+
 //BASE ARRAY CREATED^^^^^^
 //SANITIZE BASE ARRAY
 echo '<br>';
