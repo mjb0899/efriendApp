9a9f3203543bf93ae581da14d513d94e74188ccc
ADMIN <morrison0899@gmail.com>
2017-08-14 15:41:24 +0100

    creating bio page

--- eFriendMatcher.php
+++ eFriendMatcher.php
@@ -106,8 +106,10 @@ $username=$_SESSION['name'];
         <!--MIddle of container on matcher page-->
         <div class="matcherPage">
 
+            <div class="comic_font">
+                <h2 >We've Found You A Friend!!</h2>
 
-            <h2 class="comic_font">We've Found You A Friend!!</h2>
+            </div>
 
             <div class="profileInfo">
                 <div class="profile_image">
