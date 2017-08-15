9a5f4b98b06241d4bd7415f3571f57695ebb2fbc
ADMIN <morrison0899@gmail.com>
2017-08-16 00:15:33 +0100

    repair userprofile

--- updateMatrix.php
+++ updateMatrix.php
@@ -12,30 +12,37 @@ if(isset($_SESSION['name'])){
     header("url=pageNotFound.php");
 
 }
+include("dbConnect.php");
+
 //List Variables
 $bio=$_POST["bio"];
 $searchSex =$_POST["sex"]; //search
 $uid=$_SESSION['userNum'];
 $ageVal=$_POST["age"];
+if($searchSex!=null){
+    $stmt1 = $db->prepare("UPDATE user_search SET ssex=? WHERE uid=?");
+    $stmt1->bind_param('si', $searchSex, $uid);
+    $stmt1->execute();
+    $stmt1->store_result();
+    $stmt1->bind_result($col2);
 
-$stmt1 = $db->prepare("UPDATE user_search SET ssex=? WHERE uid=?");
-$stmt1->bind_param('si', $searchSex, $uid);
-$stmt1->execute();
-$stmt1->store_result();
-$stmt1->bind_result($col2);
 
+}
+
+if($ageVal!=null){
 
 $stmt1 = $db->prepare("UPDATE user_search SET sage=? WHERE uid=?");
 $stmt1->bind_param('si', $ageVal, $uid);
 $stmt1->execute();
 $stmt1->store_result();
 $stmt1->bind_result($col2);
+}
+if($bio!=null) {
 
-
-$stmt1 = $db->prepare("UPDATE user_info SET bio=? WHERE uid=?");
-$stmt1->bind_param('si', $bio, $uid);
-$stmt1->execute();
-$stmt1->store_result();
-$stmt1->bind_result($col2);
-
+    $stmt1 = $db->prepare("UPDATE user_info SET bio=? WHERE uid=?");
+    $stmt1->bind_param('si', $bio, $uid);
+    $stmt1->execute();
+    $stmt1->store_result();
+    $stmt1->bind_result($col2);
+}
 echo 1;
