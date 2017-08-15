dec80ad299d16a8d9372687d8a62f55f03c35d25
ADMIN <morrison0899@gmail.com>
2017-08-15 17:19:46 +0100

    making deny work

--- features.php
+++ features.php
@@ -58,8 +58,6 @@ if(isset($_SESSION['name'])){
                     </li>
                     <li><a href="happyFriends.php">Happy Friends</a></li>
                     <li><a href="aboutUs.php">About Us</a></li>
-                    <li><a href="sessions.php">View Active Sessions</a></li>
-
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <?php if(!isset($_SESSION['name'])) {   echo"    <li><a href='registration.php'><span class='glyphicon glyphicon-user'></span> Sign Up</a></li> "; }  ?>
