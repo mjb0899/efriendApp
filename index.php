50bb5f26aa65be992c21c1fb0dd1fb39bd788193
ADMIN <morrison0899@gmail.com>
2017-08-15 21:14:15 +0100

    syrvey design

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ if(isset($_SESSION['name'])){
                     </li>
                     <li><a href="happyFriends.php">Happy Friends</a></li>
                     <li><a href="aboutUs.php">About Us</a></li>
-                    <li><a href="eFriendSurvey.php">About Us</a></li>
+                    <li><a href="eFriendSurvey.php">survey</a></li>
 
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
