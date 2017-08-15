bbfe3c7bee4c9e593d6a9efa1b1587e425af747c
ADMIN <morrison0899@gmail.com>
2017-08-15 22:57:33 +0100

    git

--- features.php
+++ features.php
@@ -57,6 +57,7 @@ if(isset($_SESSION['name'])){
                     </li>
                     <li><a href="happyFriends.php">Happy Friends</a></li>
                     <li><a href="aboutUs.php">About Us</a></li>
+                    <li>  <a href="userProfile.php"><span class="glyphicon glyphicon-th-list"></span></a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <?php if(!isset($_SESSION['name'])) {   echo"    <li><a href='registration.php'><span class='glyphicon glyphicon-user'></span> Sign Up</a></li> "; }  ?>
