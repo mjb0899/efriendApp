2a41c589d25e0d15530102a9fe33c923432a6792
ADMIN <morrison0899@gmail.com>
2017-08-22 16:55:46 +0100

    container for features

--- features.php
+++ features.php
@@ -86,7 +86,8 @@ if(isset($_SESSION['name'])){
     <div class="container" style="height: 100%">
         <div class="wrapper_left">
             <a href="eFriendAlgo.php" class="fill-div">
-            <div class="eFriendLogo"></div>       efriend <div><p>Meet New People. Find a Friend?</p>  </div>
+            <div class="eFriendLogo"></div>       efriend <div><p>Meet New People</p>  </div>
+                <div><p>Find a Friend?</p>  </div>
            </a>
 
 
@@ -99,7 +100,7 @@ if(isset($_SESSION['name'])){
                 <div class="eConnectLogo"></div>  eConnect
 
                 <div><p>Find a Connection?</p>  </div>
-                <div><p>Anonymous?</p>  </div>
+                <div><p>Anonymous? <br> Reveal yourself if you think the connection is right!</p>  </div>
 
             </a>
 
