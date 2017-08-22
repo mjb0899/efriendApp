2cd56b9fa16d3badfc540c28d7db25c6437f7128
ADMIN <morrison0899@gmail.com>
2017-08-22 18:46:08 +0100

    container for features

--- messages.php
+++ messages.php
@@ -354,7 +354,7 @@ $get_mid=$_SESSION['mid'];//match id
 
                             </div>
                         </div>
-                                <div class="absol">
+                                <div>
                                     <div class="form-group area_left">
                                         <textarea class="form-control"  rows="3" id="msgSend" maxlength="100"
                                                   <?php if(!isset($_SESSION['mid'])) {   echo " disabled";}?>></textarea>
