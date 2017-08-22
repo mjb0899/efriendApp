2b225fce557926801ee74a00f47cc04d1cdd8a6c
ADMIN <morrison0899@gmail.com>
2017-08-22 18:48:27 +0100

    align upload

--- messages.php
+++ messages.php
@@ -322,37 +322,7 @@ $get_mid=$_SESSION['mid'];//match id
 
                     <div class="text_input">
                         <div style="width: 100%;">
-                            <div style="float:right;">
 
-
-                                <div class="container">
-                                    <!-- Trigger the modal with a button -->
-                                   <button type="button" class="btn-place" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-picture"></button>
-
-                                    <!-- Modal -->
-                                    <div class="modal fade" id="myModal" role="dialog">
-                                        <div class="modal-dialog modal-sm">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                    <h4 class="modal-title">Upload Profile Image</h4>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <form action="convoImage.php" method="post" enctype="multipart/form-data">
-                                                        <input type="file" name="file" id="exampleInputFile" required>
-                                                        <button type="submit" class="btn btn-default btn-sm" name="submit"> <span class="glyphicon glyphicon-pencil">Upload</span></button>
-                                                    </form>
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                            </div>
                         </div>
                                 <div>
                                     <div class="form-group area_left">
@@ -365,6 +335,38 @@ $get_mid=$_SESSION['mid'];//match id
 
                                     <div class="form-group area_right">
                                     <button type="submit" class="btn btn-info btn-lg" onclick="return send()">>></button>
+
+
+                                    </div>
+                                    <div style="float:right;">
+
+
+                                        <div class="container">
+                                            <!-- Trigger the modal with a button -->
+                                            <button type="button" class="btn-place" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-picture"></button>
+
+                                            <!-- Modal -->
+                                            <div class="modal fade" id="myModal" role="dialog">
+                                                <div class="modal-dialog modal-sm">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                            <h4 class="modal-title">Upload Profile Image</h4>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <form action="convoImage.php" method="post" enctype="multipart/form-data">
+                                                                <input type="file" name="file" id="exampleInputFile" required>
+                                                                <button type="submit" class="btn btn-default btn-sm" name="submit"> <span class="glyphicon glyphicon-pencil">Upload</span></button>
+                                                            </form>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
 
                                     </div>
                                 </div>
