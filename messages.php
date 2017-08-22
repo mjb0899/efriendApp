bbae631a5e9f763aaeff7c169fa0cf267046dd49
ADMIN <morrison0899@gmail.com>
2017-08-22 19:22:38 +0100

    repai messages

--- messages.php
+++ messages.php
@@ -318,36 +318,40 @@ $get_mid=$_SESSION['mid'];//match id
 
 
                         </div><!--MATTER sccroll CLOSE  TAG-->
+
+           <div style="clear: both">
+               <div class="container">
+                   <!-- Trigger the modal with a button -->
+                   <button type="button" class="btn-place" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-picture"></button>
+
+                   <!-- Modal -->
+                   <div class="modal fade" id="myModal" role="dialog">
+                       <div class="modal-dialog modal-sm">
+                           <div class="modal-content">
+                               <div class="modal-header">
+                                   <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                   <h4 class="modal-title">Upload Profile Image</h4>
+                               </div>
+                               <div class="modal-body">
+                                   <form action="convoImage.php" method="post" enctype="multipart/form-data">
+                                       <input type="file" name="file" id="exampleInputFile" required>
+                                       <button type="submit" class="btn btn-default btn-sm" name="submit"> <span class="glyphicon glyphicon-pencil">Upload</span></button>
+                                   </form>
+                               </div>
+                               <div class="modal-footer">
+                                   <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
            <div class="text_input">
                <div style="width: 100%;">
                    <div style="float:right;">
 
 
-                       <div class="container">
-                           <!-- Trigger the modal with a button -->
-                           <button type="button" class="btn-place" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-picture"></button>
 
-                           <!-- Modal -->
-                           <div class="modal fade" id="myModal" role="dialog">
-                               <div class="modal-dialog modal-sm">
-                                   <div class="modal-content">
-                                       <div class="modal-header">
-                                           <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                           <h4 class="modal-title">Upload Profile Image</h4>
-                                       </div>
-                                       <div class="modal-body">
-                                           <form action="convoImage.php" method="post" enctype="multipart/form-data">
-                                               <input type="file" name="file" id="exampleInputFile" required>
-                                               <button type="submit" class="btn btn-default btn-sm" name="submit"> <span class="glyphicon glyphicon-pencil">Upload</span></button>
-                                           </form>
-                                       </div>
-                                       <div class="modal-footer">
-                                           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                       </div>
-                                   </div>
-                               </div>
-                           </div>
-                       </div>
 
 
                    </div>
