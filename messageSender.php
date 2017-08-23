d9b0cc8b3cd58a8cdb6d56e0c0da9f7a4d0557ef
ADMIN <morrison0899@gmail.com>
2017-08-23 19:37:20 +0100

    index remove

--- messageSender.php
+++ messageSender.php
@@ -13,11 +13,15 @@ $match=$_SESSION['check'];//match
 $mid=$_SESSION['mid'];//mid
 $cuser=$_SESSION['name'];
 //sanitize
+
+$text_san=htmlspecialchars($text);
+
+
 include ("dbConnect.php");
 if(isset($_POST['msg'])){
 
     $stmt2 = $db->prepare("INSERT into convo(content,mid,sender,start_date) VALUES (?,?,?,?)");
-    $stmt2->bind_param('siss', $text,$mid ,$cuser,$date);
+    $stmt2->bind_param('siss', $text_san,$mid ,$cuser,$date);
     $stmt2->execute();
     $stmt2->store_result();
     $stmt2->bind_result($col1);
