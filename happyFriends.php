4d7344d93a0e88630cfbec752bae4ac7b18dec1a
ADMIN <morrison0899@gmail.com>
2017-08-25 16:53:14 +0100

    happy friends

--- happyFriends.php
+++ happyFriends.php
@@ -80,12 +80,46 @@ session_start();
 <!--MAIN CODE-->
 <main>
 
-
-
-
     <div class="container_reg">
         <div class="wrapper_up">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
 
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
+
+                <!-- Wrapper for slides -->
+
+                <div class="item active">
+                         <img src="images/mypic.png" id="#pic2" alt="Los Angeles" >
+                     </div>
+
+                     <div class="item">
+                         <img src="images/mypic.png" id="#pic2" alt="Chicago" >
+                     </div>
+
+                     <div class="item">
+                         <img src="images/mypic.png" id="#pic2" alt="New york" >
+                     </div>
+
+
+
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <!--CAROUSEL END-->
 
 
 
