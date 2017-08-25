374915eb707038d2503b1fe4c53863d38a929cda
ADMIN <morrison0899@gmail.com>
2017-08-25 23:50:40 +0100

    features bar

--- aboutUs.php
+++ aboutUs.php
@@ -38,10 +38,10 @@ session_start();
 
 
 
-
+                    <?php if(isset($_SESSION['name'])) {   echo'
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" href="features.php">Features <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
+                        <ul class="dropdown-menu">';}?>
 
                             <?php if(isset($_SESSION['name'])) {   echo"       <li><a href='eFriendAlgo.php'>eFriend Finder</a></li>"; }  ?>
                             <?php if(!isset($_SESSION['name'])) {   echo" "; }  ?>
@@ -50,9 +50,11 @@ session_start();
                             <?php if(isset($_SESSION['name'])) {   echo"           <li><a href='eConnectAlgo.php'>eConnect</a></li>"; }  ?>
                             <?php if(!isset($_SESSION['name'])) {   echo""; }  ?>
 
-
+    <?php if(isset($_SESSION['name'])) {   echo'
                         </ul>
-                    </li>
+                    </li>';}?>
+
+
                     <li><a href="happyFriends.php">Happy Friends</a></li>
                     <li class="active"><a href="aboutUs.php">About Us</a></li>
                     <?php if(isset($_SESSION['name'])) {   echo"   <li>  <a href='messages.php'>Inbox</span></a></li> "; }  ?>
