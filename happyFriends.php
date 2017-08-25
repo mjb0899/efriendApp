c831a64a867614cf6dc079aeb33d5610c78d6c06
ADMIN <morrison0899@gmail.com>
2017-08-25 16:54:05 +0100

    happy friends

--- happyFriends.php
+++ happyFriends.php
@@ -82,8 +82,9 @@ session_start();
 
     <div class="container_reg">
         <div class="wrapper_up">
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-
+            <div class="container">
+                <h2>Carousel Example</h2>
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
                     <!-- Indicators -->
                     <ol class="carousel-indicators">
                         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -91,36 +92,32 @@ session_start();
                         <li data-target="#myCarousel" data-slide-to="2"></li>
                     </ol>
 
-                <!-- Wrapper for slides -->
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="la.jpg" alt="Los Angeles" style="width:100%;">
+                        </div>
 
-                <div class="item active">
-                         <img src="images/mypic.png" id="#pic2" alt="Los Angeles" >
-                     </div>
+                        <div class="item">
+                            <img src="chicago.jpg" alt="Chicago" style="width:100%;">
+                        </div>
 
-                     <div class="item">
-                         <img src="images/mypic.png" id="#pic2" alt="Chicago" >
-                     </div>
+                        <div class="item">
+                            <img src="ny.jpg" alt="New york" style="width:100%;">
+                        </div>
+                    </div>
 
-                     <div class="item">
-                         <img src="images/mypic.png" id="#pic2" alt="New york" >
-                     </div>
-
-
-
+                    <!-- Left and right controls -->
+                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
-
-                <!-- Left and right controls -->
-                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
-            <!--CAROUSEL END-->
-
 
 
 
