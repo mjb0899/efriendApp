a937386b77bcf16b95ad3110a2e625129bf70177
ADMIN <morrison0899@gmail.com>
2017-08-25 19:55:13 +0100

    happy friends

--- features.php
+++ features.php
@@ -58,7 +58,7 @@ if(isset($_SESSION['name'])){
                     <li><a href="happyFriends.php">Happy Friends</a></li>
                     <li><a href="aboutUs.php">About Us</a></li>
                     <li>  <a href="messages.php">Inbox<span class="glyphicon glyphicon-envelopet"></span></a></li>
-                    <li> <a href="survey.php">My Profile<span class=" glyphicon glyphicon-tasks"></span></a></li>
+                    <li> <a href="userProfile.php">My Profile<span class=" glyphicon glyphicon-tasks"></span></a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <?php if(!isset($_SESSION['name'])) {   echo"    <li><a href='registration.php'><span class='glyphicon glyphicon-user'></span> Sign Up</a></li> "; }  ?>
