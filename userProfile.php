c71bf9a876806689d3ab592d0aba698bd1587e73
ADMIN <morrison0899@gmail.com>
2017-08-25 19:52:07 +0100

    happy friends

--- userProfile.php
+++ userProfile.php
@@ -235,7 +235,6 @@ if(isset($_SESSION['name'])){
             </div>
             <div class="collapse navbar-collapse" id="myNavbar">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="userProfile.php">Home</a></li>
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" href="features.php">Features <span class="caret"></span></a>
                         <ul class="dropdown-menu">
