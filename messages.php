2236b1e2f8ffc2bf8a2e9172407bb1e5b075c0ca
ADMIN <morrison0899@gmail.com>
2017-08-26 20:45:59 +0100

    matter get border

--- messages.php
+++ messages.php
@@ -328,7 +328,7 @@ $get_mid=$_SESSION['mid'];//match id
 
            <div class="uploader" style="clear: both;width: 100%;">
 
-                   <div class="container" style="float: left;width: 40%;border-left:2px solid #cccccc;">
+                   <div class="container" style="float: left;width: 40%;border-left:2px solid #cccccc;background: rgba(25, 25, 25, .7);">
                        <!-- Trigger the modal with a button -->
                        <button type="button" class="btn-place btn btn-default" data-toggle="modal"data-target="#myModal"   <?php if(!isset($_SESSION['mid'])) {   echo " disabled";}?>><span class="glyphicon glyphicon-picture">Attachment</button>
 
@@ -354,7 +354,7 @@ $get_mid=$_SESSION['mid'];//match id
                        </div>
                    </div>
 
-              <div style="float: right;width: 50%;"><p id="showthis"><?php echo $_SESSION['check']?></p></div>
+              <div style="float: right;width: 50%;background: rgba(25, 25, 25, .7);"><p id="showthis"><?php echo $_SESSION['check']?></p></div>
 
 
 
